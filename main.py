1907d23153ef0a6ed146e2dbc5adc0a1fb1c812e
zntb <zenetibi@yahoo.com>
2025-07-12 11:55:46 +0300

    Add Footer

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import pandas as pd
 
 # Initialize PDF
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
 
+    # Set the header
     # Set font (required before adding text)
     pdf.set_font("helvetica", size=24, style="B")
     pdf.set_text_color(100, 100, 100)
@@ -26,8 +28,20 @@ for index, row in df.iterrows():
     )
     pdf.line(10, 22, 200, 22)
     
+    
+    # Set the footer
+    pdf.ln(265)
+    pdf.set_font("helvetica", size=8, style="I")
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, text=row["Topic"], border=0, align="R", new_x=XPos.RMARGIN, new_y=YPos.NEXT)
+    
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        # Set the footer
+        pdf.ln(277)
+        pdf.set_font("helvetica", size=8, style="I")
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, text=row["Topic"], border=0, align="R", new_x=XPos.RMARGIN, new_y=YPos.NEXT)
 
 # Modern output() usage without deprecated "dest" parameter:
 pdf.output("output.pdf")  # Removed "F" parameter
